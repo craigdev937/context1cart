1d710109b6a6024013d897f9baf08a435e1d3139
Craig <indigomx9@gmail.com>
2022-04-20 13:32:52 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Shopping Cart
+
+Initial setup of the project.  Created with React 18 and React Router 6.
+
+
+
+
---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque eaque odit itaque alias harum perspiciatis necessitatibus accusantium consequatur quibusdam quam culpa temporibus, sit natus iste magnam quod nihil adipisci at.</p>
+        </React.Fragment>
+    );
+};
+
+
+
